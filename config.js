405ae76482ee72d37e675a31bba160e9332a19df
Jash Mistry <99473238+jash413@users.noreply.github.com>
2024-05-29 11:44:22 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+const network = {
+    serverUrl : 'http://localhost:3000/',
+    onlineUrl : 'https://backend-api-topaz.vercel.app/',
+    token : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEwMDAwMSwiaWF0IjoxNzE2ODM5ODUzfQ.cAWvcoEqmyLN1FOuthUabZKO6LsXwCOrtcmrw32NETc'
+};
+
+export default network;
